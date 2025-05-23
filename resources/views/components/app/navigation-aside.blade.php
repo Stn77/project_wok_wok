6ed845e1b89b1b7027a1d30b9fe4ae1d1035403c
Stn77 <fudinyusuf20@gmail.com>
2025-05-23 15:00:50 +0700

    Awesome dark mode, component

--- resources/views/components/app/navigation-aside.blade.php
+++ resources/views/components/app/navigation-aside.blade.php
@@ -1,3 +1,7 @@
-<li>
-    <a {{$attributes}} class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Jurusan</a>
-</li>
+<a {{$attributes}}
+    class=" {{$check ? 'bg-gray-100 dark:bg-gray-600' : ''}} flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700 group">
+    <span class="material-icons">
+        {{$icon}}
+    </span>
+    <span class="flex-1 ms-3 whitespace-nowrap">{{$name}}</span>
+</a>
---
+++ resources/views/users/admin/user/kelas-jurusan.blade.php
@@ -0,0 +1,5 @@
+<x-app.app>
+     <div class="w-full h-full bg-gray-200 dark:bg-gray-700">
+          
+     </div>
+</x-app.app>
